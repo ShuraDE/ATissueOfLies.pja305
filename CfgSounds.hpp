b08ffb38accc7bbb8da745ef935da1e42c97462c
ShuraDE <mail@kev-in.net>
2015-08-13 19:05:51 +0200

    add no sound

--- CfgSounds.hpp
+++ CfgSounds.hpp
@@ -1,26 +1,38 @@
+class CfgSFX {
+	sounds[] = {};
+	class sfx_mistake_music {
+		name = "sfx_mistake_music";
+		sounds[]={sound1};
+		sound1[]={"snd\music.ogg",1,1,50,1,0,0,0};
+		empty[]= {"",0,0,0,0,0,0,0};
+	};
+};
 
 class CfgSounds
 {
-  sounds[] = {snd_radio_indo, snd_mistake_music, snd_video_intro};
+  sounds[] = {snd_radio_indo, snd_mistake_music, snd_video_intro, NoSound};
 
-  class snd_radio_indo
-  {
+  class snd_radio_indo {
     name = "ADL_ATOL_SND_RADIO_INDO";
     sound[] = {"snd\indo_radio.ogg", "db+1", 1};
     titles[] = {0, ""};
   };  
 
-  class snd_mistake_music
-  {
+  class snd_mistake_music {
     name = "ADL_ATOL_SND_MUSIC";
-    sound[] = {"\snd\music.ogg", "db+1", 1};
+    sound[] = {"snd\music.ogg", "db+1", 1};
     titles[] = {1, "next time more luck ;)"};
   };  
   
-  class snd_video_intro
-  {
+  class snd_video_intro {
     name = "ADL_ATOL_SND_VIDEO_INTRO";
-    sound[] = {"\media\intro_audio.ogg", "db+1", 1};
+    sound[] = {"media\intro_audio.ogg", "db+1", 1};
     titles[] = {0, ""};
+  };
+  
+  class NoSound {
+	name = "NoSound";
+	sound[] = {"", 0, 1};
+	titles[] = {};
   };
 };
