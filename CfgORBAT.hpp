b5ea746cb90067305756f9fcd32b200cf0abd905
ShuraDE <mail@kev-in.net>
2015-09-20 09:07:32 +0200

    clean up comments

--- CfgORBAT.hpp
+++ CfgORBAT.hpp
@@ -61,33 +61,23 @@ class CfgORBAT
 	};
 	class UFOXHOUND
 	{
-		id = 10; // Unit ID
-		idType = 2; // Unit ID type
-		 // 0 - show as ordinal number, e.g. "7th" (default)
-		 // 1 - show as roman numeral, e.g. "VII"
-		 // 2 - show as NATO phonetical alphabet word, e.g. "Golf"
-         // 3 - show as color, e.g. "Red"
-
-		side = "West"; // Unit side from CfgChainOfCommand >> Sides
-		size = "Troop"; // Unit size from CfgChainOfCommand >> Sizes
-		type = "Infantry"; // Unit type from CfgChainOfCommand >> Types
-		insignia = "pic\orbat\marsoc.paa"; // Unit insignia, displayed in ORBAT background and in tooltip
-		colorInsignia[] = {0,0,1,1}; // Insignia image color (white default), can be either RGBA array, or class from CfgMarkerColors
-		commander = "NATOMen"; // Name of unit commander. Can be either direct name, or class from CfgWorlds >> GenericNames (random name from the list is then selected)
-		commanderRank = "Captain"; // Rank of unit commander (by default selected based on unit size)
-		tags[] = {}; // Group tags. When opening ORBAT viewer, user can define what tags will be accepted.
-		text = "Marine Special Operations Regiment"; // Custom text and short text, can still use some arguments when defined:
-		// 	%1 - ID (e.g. "7th")
-		// 	%2 - Type (e.g. "Infantry")
-		// 	%3 - Size (e.g. "Division")
+		id = 10;
+		idType = 2;
+		side = "West";
+		size = "Troop";
+		type = "Infantry";
+		insignia = "pic\orbat\marsoc.paa";
+		colorInsignia[] = {0,0,1,1};
+		commander = "NATOMen";
+		commanderRank = "Captain";
+		tags[] = {};
+		text = "Marine Special Operations Regiment";
 		textShort = "MSOR";
 		//texture = "\ca\missions_f\data\orbat\customTexture_ca.paa"; // Custom texture, will replace icon set by 'type' param.
-		color[] = {0,0,1,1}; // Custom color, will replace color set by 'side' param. Can be either RGBA array, or class from CfgMarkerColors
-		description= "Foxhound Leader"; // A brief description of the group or unit.
-		assets[] = {B_Boat_Transport_01_F}; // Will display pictures of all available assets to the unit commander
-		subordinates[] = {}; // Subordinates, searched on the same level as this class.
-
-        // When 'subordinates' are missing, child classes will be used. They can have their own subs - number of tiers is not limited.
+		color[] = {0,0,1,1};
+		description= "Foxhound Leader";
+		assets[] = {B_Boat_Transport_01_F};
+		subordinates[] = {};
 		class UFOXHOUND_1
 		{
 			id = 1;
@@ -111,30 +101,21 @@ class CfgORBAT
 	};
 	class UACORN
 	{
-		id = 1; // Unit ID
-		idType = 3; // Unit ID type
-		 // 0 - show as ordinal number, e.g. "7th" (default)
-		 // 1 - show as roman numeral, e.g. "VII"
-		 // 2 - show as NATO phonetical alphabet word, e.g. "Golf"
-         // 3 - show as color, e.g. "Red"
-
-		side = "West"; // Unit side from CfgChainOfCommand >> Sides
-		size = "Troop"; // Unit size from CfgChainOfCommand >> Sizes
-		type = "Infantry"; // Unit type from CfgChainOfCommand >> Types
-		insignia = "pic\orbat\marsoc.paa"; // Unit insignia, displayed in ORBAT background and in tooltip
-		colorInsignia[] = {0,0,1,1}; // Insignia image color (white default), can be either RGBA array, or class from CfgMarkerColors
-		commander = "NATOMen"; // Name of unit commander. Can be either direct name, or class from CfgWorlds >> GenericNames (random name from the list is then selected)
-		commanderRank = "Major"; // Rank of unit commander (by default selected based on unit size)
-		tags[] = {}; // Group tags. When opening ORBAT viewer, user can define what tags will be accepted.
-		text = "Marine Special Operations Support Group"; // Custom text and short text, can still use some arguments when defined:
-		// 	%1 - ID (e.g. "7th")
-		// 	%2 - Type (e.g. "Infantry")
-		// 	%3 - Size (e.g. "Division")
+		id = 1;
+		idType = 3;
+		side = "West";
+		size = "Troop";
+		type = "Infantry";
+		insignia = "pic\orbat\marsoc.paa";
+		colorInsignia[] = {0,0,1,1};
+		commander = "NATOMen";
+		commanderRank = "Major";
+		tags[] = {};
+		text = "Marine Special Operations Support Group";
 		textShort = "MSOG";
-		//texture = "\ca\missions_f\data\orbat\customTexture_ca.paa"; // Custom texture, will replace icon set by 'type' param.
-		color[] = {0,0,1,1}; // Custom color, will replace color set by 'side' param. Can be either RGBA array, or class from CfgMarkerColors
-		description= "MSOG Officer"; // A brief description of the group or unit.
-		assets[] = {}; // Will display pictures of all available assets to the unit commander
-		subordinates[] = {}; // Subordinates, searched on the same level as this class.
+		color[] = {0,0,1,1};
+		description= "MSOG Officer";
+		assets[] = {};
+		subordinates[] = {};
 	};
 };
